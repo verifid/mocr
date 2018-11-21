f2e47bdd687c27edc6f97b06ede2f82baf59509f
Abdullah Selek <abdullahselek@gmail.com>
2018-11-21 19:27:44 +0000

    New function which loads image.

--- mocr/text_recognition.py
+++ mocr/text_recognition.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import cv2
 
 class TextRecognizer(object):
     """TextRecognizer can be used as to detect meaningful optical characters from identity cards.
@@ -36,3 +37,13 @@ class TextRecognizer(object):
         self.width = width
         self.height = height
         self.padding = padding
+
+    def load_image(self):
+        """Load the input image and grab the image dimensions.
+        Returns:
+          (original, original_height, original_width): Tuple of image, it's height and width.
+        """
+        image = cv2.imread(self.image_path)
+        original = image.copy()
+        (original_height, original_width) = image.shape[:2]
+        return (original, original_height, original_width)
--- tests/test_text_recognition.py
+++ tests/test_text_recognition.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import unittest
 import pytest
 from mocr import TextRecognizer
@@ -8,8 +9,15 @@ from mocr import TextRecognizer
 class TextRecognizerTest(unittest.TestCase):
 
     def setUp(self):
-        self._text_recognizer = TextRecognizer('data/sample_uk_identity_card.png')
+        self._image_path = os.path.join('tests', 'data/sample_uk_identity_card.png')
+        self._text_recognizer = TextRecognizer(self._image_path)
 
     def test_init(self):
         self.assertIsNotNone(self._text_recognizer)
-        self.assertEqual(self._text_recognizer.image_path, 'data/sample_uk_identity_card.png')
+        self.assertEqual(self._text_recognizer.image_path, self._image_path)
+
+    def test_load_image(self):
+        (original, original_height, original_width) = self._text_recognizer.load_image()
+        self.assertIsNotNone(original)
+        self.assertEqual(original_height, 201)
+        self.assertEqual(original_width, 312)
