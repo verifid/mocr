780c042710ce146377caf8c541e0df0acaf951ed
Abdullah Selek <abdullahselek@gmail.com>
2018-11-28 21:06:30 +0000

    Fix for module name.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def extract_metaitem(meta):
     raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
 
 setup(
-    name='totppy',
+    name='mocr',
     version=extract_metaitem('version'),
     license=extract_metaitem('license'),
     description=extract_metaitem('description'),
