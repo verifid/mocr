e1378b13d6f14aba04c643847cff68061b192d6d
Abdullah Selek <abdullahselek@gmail.com>
2019-01-08 22:44:04 +0000

    Updated download url.

--- mocr/__init__.py
+++ mocr/__init__.py
@@ -11,7 +11,7 @@ __copyright__    = 'Copyright (c) 2018 Abdullah Selek'
 __license__      = 'MIT License'
 __version__      = '0.1.1'
 __url__          = 'https://github.com/verifid/mocr'
-__download_url__ = 'https://github.com/verifid/mocr'
+__download_url__ = 'https://pypi.org/project/mocr/'
 __description__  = 'Meaningful Optical Character Recognition from identity cards with Deep Learning.'
 
 from .text_recognition import TextRecognizer
