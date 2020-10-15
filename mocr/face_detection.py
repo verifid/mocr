cec8d70b81469a454ed1c0f3712ae6a4f2b6edce
Abdullah Selek <abdullahselek@gmail.com>
2020-10-15 11:30:49 +0100

    Introduce face detection from video

--- mocr/face_detection.py
+++ mocr/face_detection.py
@@ -39,3 +39,57 @@ def detect_face(image_path: str) -> bytearray:
     (x, y, w, h) = faces[0]
     cropped_face_image = image[y:y+h, x:x+w]
     return cropped_face_image
+
+def detect_face_from_video(video_path: str) -> bytearray:
+    """Detect face from given video path.
+    Args:
+      video_path (str):
+        Path to input video on file system.
+    Returns:
+      image (bytes array):
+        Bytes array for detected face image.
+    """
+
+    if not os.path.isfile(video_path):
+        print('mocr:face_detection:detect_face_from_video No video found on given video path!')
+        return None
+
+    # Create the haar cascade
+    cascade_path = os.path.join(os.path.dirname(__file__), 'haarcascades/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cascade_path)
+
+    video_capture = cv2.VideoCapture(video_path)
+    faces = []
+    cropped_face_image = None
+    frame = None
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            break
+
+        if len(faces) == 1:
+            break
+
+    if len(faces) == 0 or len(faces) > 1:
+        print('mocr:face_detection:detect_face_from_video Video should does not contain a face!')
+        return None
+
+    (x, y, w, h) = faces[0]
+    cropped_face_image = frame[y:y+h, x:x+w]
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+    return cropped_face_image
--- tests/test_face_detection.py
+++ tests/test_face_detection.py
@@ -15,7 +15,7 @@ class FaceDetectionTest(unittest.TestCase):
         self.assertIsNotNone(face_image)
 
     
-    def test_detect_face_fail(self):
+    def test_detect_face_fails(self):
         image_path = os.path.join(os.path.dirname(__file__), 'data/test.png')
         face_image = face_detection.detect_face(image_path)
         self.assertIsNone(face_image)
@@ -23,10 +23,21 @@ class FaceDetectionTest(unittest.TestCase):
         face_image = face_detection.detect_face(image_path)
         self.assertIsNone(face_image)
 
+    def test_detect_face_from_video_success(self):
+        video_path = os.path.join(os.path.dirname(__file__), 'data/face-demographics-walking.mp4')
+        face_image = face_detection.detect_face_from_video(video_path)
+        self.assertIsNotNone(face_image)
+
+    def test_detect_face_from_video_fails(self):
+        video_path = os.path.join(os.path.dirname(__file__), 'data/unavailable.mp4')
+        face_image = face_detection.detect_face_from_video(video_path)
+        self.assertIsNone(face_image)
 
     def main(self):
         self.test_detect_face_success()
-        self.test_detect_face_fail()
+        self.test_detect_face_fails()
+        self.test_detect_face_from_video_success()
+        self.test_detect_face_from_video_fails()
 
 
 if __name__ == "__main__":
